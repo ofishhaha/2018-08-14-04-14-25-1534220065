bb42b7671d1676dba50909d87947cd83c6b20551
Your Name <you@example.com>
2019-05-21 21:00:58 +0800

    modified LCD

--- main/main.js
+++ main/main.js
@@ -3,19 +3,25 @@ module.exports = function main(a) {
     var middle_LCD = ["|.|","..|","._|","._|","|_|","|_.","|_.","..|","|_|","|_|"];
     var down_LCD = ["|_|","..|","|_.","._|","..|","._|","|_|","..|","|_|","..|"];
     
-    var out_up = ""
-    var out_middle = ""
-    var out_down = ""
+    var out_up = "";
+    var out_middle = "";
+    var out_down = "";
 
     for(var i =0;i<a.length;i++)
     {
-        out_up += up_LCD[parseInt(a[i])]+" ";
-        out_middle += middle_LCD[parseInt(a[i])]+" ";
-        out_down += down_LCD[parseInt(a[i])]+" "
+        if(i!==a.length-1)
+        {
+	   out_up += up_LCD[parseInt(a[i])]+" ";
+           out_middle += middle_LCD[parseInt(a[i])]+" ";
+           out_down += down_LCD[parseInt(a[i])]+" ";
+        }
+        else
+        {
+	   out_up += up_LCD[parseInt(a[i])]+"\n";
+           out_middle += middle_LCD[parseInt(a[i])]+"\n";
+           out_down += down_LCD[parseInt(a[i])]+"\n";
+        }
     }
-    out_up[-1]="\n"
-    out_middle[-1]="\n"
-    out_down[-1]="\n"
-
+    
     return out_up+out_middle+out_down
-};
+}
