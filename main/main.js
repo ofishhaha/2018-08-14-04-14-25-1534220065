1486881303041aad5afc30a42e7b3b3f94d16a35
Your Name <you@example.com>
2019-05-21 20:09:39 +0800

    LCD

--- main/main.js
+++ main/main.js
@@ -1,4 +1,21 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(a) {
+    var up_LCD = ["._.","...","._.","._.","...","._.","._.","._.","._.","._."];
+    var middle_LCD = ["|.|","..|","._|","._|","|_|","|_.","|_.","..|","|_|","|_|"];
+    var down_LCD = ["|_|","..|","|_.","._|","..|","._|","|_|","..|","|_|","..|"];
+    
+    var out_up = ""
+    var out_middle = ""
+    var out_down = ""
+
+    for(var i =0;i<a.length;i++)
+    {
+        out_up += up_LCD[parseInt(a[i])]+" ";
+        out_middle += middle_LCD[parseInt(a[i])]+" ";
+        out_down += down_LCD[parseInt(a[i])]+" "
+    }
+    out_up[-1]="\n"
+    out_middle[-1]="\n"
+    out_down[-1]="\n"
+
+    return out_up+out_middle+out_down
 };
